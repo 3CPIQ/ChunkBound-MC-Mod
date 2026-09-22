e79b0afccf8f03b66d3c2064aec4f0e113ee96d7
Xoy <joaoebatistasouza@gmail.com>
2026-09-22 13:54:02 -0300

    Add src/main/java/com/jxmann/chunkbound/ScaleSavedData.java

---
+++ src/main/java/com/jxmann/chunkbound/ScaleSavedData.java
@@ -0,0 +1,49 @@
+package com.jxmann.chunkbound;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.saveddata.SavedData;
+
+public final class ScaleSavedData extends SavedData {
+    private static final String FILE_NAME = "chunkbound_scale";
+    private final ScaleSelection selection;
+
+    private ScaleSavedData(ScaleSelection selection) {
+        this.selection = selection;
+        setDirty();
+    }
+
+    public static ScaleSavedData get(MinecraftServer server) {
+        return server.overworld().getDataStorage().computeIfAbsent(
+            ScaleSavedData::load,
+            () -> new ScaleSavedData(PendingScale.take()),
+            FILE_NAME
+        );
+    }
+
+    public static ScaleSavedData load(CompoundTag tag) {
+        ScaleMode mode;
+        try {
+            mode = ScaleMode.valueOf(tag.getString("mode"));
+        } catch (IllegalArgumentException exception) {
+            mode = ScaleMode.CLASSIC;
+        }
+        int width = Math.max(1, tag.getInt("width"));
+        int length = Math.max(1, tag.getInt("length"));
+        boolean otherDimensions = tag.getBoolean("otherDimensions");
+        return new ScaleSavedData(new ScaleSelection(mode, width, length, otherDimensions));
+    }
+
+    public ScaleSelection selection() {
+        return selection;
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag tag) {
+        tag.putString("mode", selection.mode().name());
+        tag.putInt("width", selection.width());
+        tag.putInt("length", selection.length());
+        tag.putBoolean("otherDimensions", selection.otherDimensions());
+        return tag;
+    }
+}
